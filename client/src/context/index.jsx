26e8d88843f6df2f592f82cb7c1384316129813b
Sarnika K <sharu022kemp@gmail.com>
2024-04-28 17:13:53 +0530

    destructure error

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -1,6 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext(null);
 export default function GlobalState({ children }) {
+
+  const [formdata, setFormData] = useState({
+    title: "",
+    description : ""
+  })
+
   return <GlobalContext.Provider>{children}</GlobalContext.Provider>;
 }
--- client/src/pages/add-blog/index.jsx
+++ client/src/pages/add-blog/index.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import classes from "./styles.module.css";
+import { GlobalContext } from "../../context";
 
 export default function AddNewBlog() {
+  const {formdata, setFormData} = useContext(GlobalContext);
+
+  console.log(formdata);
+
   return (
     <div className={classes.wrapper}>
       <h1>Add a blog</h1>
@@ -10,11 +16,19 @@ export default function AddNewBlog() {
           name="title"
           id="title"
           placeholder="Enter blog title"
+          value={formdata.title}
+          onChange={(event) =>
+            setFormData({ ...formdata, title: event.target.value })
+          }
         />
         <textarea
           name="description"
           id="description"
           placeholder="enter description"
+          value={formdata.description}
+          onChange={(event) =>
+            setFormData({ ...formdata, description: event.target.value })
+          }
         />
         <button>Add new Blog</button>
       </div>
